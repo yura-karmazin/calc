22b0b6f9dcead4b343aa5d081f641d7bbd457dc1
yura-karmazin <58439049+yura-karmazin@users.noreply.github.com>
2019-12-08 20:24:54 +0300

    добавил метод, который считывает число не пропускает символы

--- Calculate.java
+++ Calculate.java
@@ -1,13 +1,13 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Calculate {
     public static void main (String[] args){
         System.out.println("Please input first number: ");
-        Scanner scanner= new Scanner(System.in);
-        double num1 = scanner.nextInt();
-        System.out.println("Your input ("+ num1 + ") as a first number");
+        double num1 = getDouble();
+        System.out.println("Your input (" + num1 + ") as a first number");
         System.out.println("Pleas input second number: ");
-        double num2 = scanner.nextInt();
+        double num2 = getDouble();
         System.out.println("Your input ("+ num2 + ") as a second number");
         double sum = num1 + num2;
         System.out.println("*******************************************");
@@ -26,7 +26,17 @@ public class Calculate {
         }
         System.out.println("*******************************************");
     }
+    public static double getDouble() {
+        Scanner scanner= new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
+        double num;
+        if (scanner.hasNextDouble()) {
+            num = scanner.nextDouble();
+        } else {
+            System.out.println("Please enter number");
+            scanner.next();//рекурсия
+            num = getDouble();
+        }
+        return num;
+    }
 }
-// test commit
-// ewrwerewr
-//sdfgh
